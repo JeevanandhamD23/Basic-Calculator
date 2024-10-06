ce0a6470d72abbf27715c81f6a3f882bd1b7d620
JeevanandhamD <djeevanandham553@gmail.com>
2024-10-06 12:51:46 +0530

    add calci code

---
+++ calciScript.js
@@ -0,0 +1,35 @@
+
+
+function enterMethod (value){
+  console.log(document.getElementsByClassName('input')[0].value)
+  document.getElementsByClassName('input')[0].value += value
+}
+
+function calculate(){
+    let a = document.getElementsByClassName('input')[0].value;
+    let b = math.evaluate(a);
+    document.getElementsByClassName('input')[0].value = b;
+}
+
+function clearInput(){
+  document.getElementsByClassName('input')[0].value = ''
+}
+
+
+
+const calculatorKeyArray = ['1','2','3','4','5','6','7','8','9','0','/','*','-','+','.','Backspace']
+document.onkeyup = function (event){
+    console.log( event.key)
+    if (event.keyCode === 13) { 
+        console.log("Enter"); 
+        let x = document.getElementsByClassName("input")[0].value 
+        console.log(x); 
+        calculate(); 
+    } else if(calculatorKeyArray.includes(event.key)){
+        enterMethod(event.key)
+    }
+    else{
+        window.alert('Please Enter Valid Input')
+    }
+
+}
